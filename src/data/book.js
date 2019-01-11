18ac780add7ee470cad0bda4d1211cd4bf277a44
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-01-10 20:53:35 -0600

    Added chapter five text. Fixed chapter four.

--- src/data/book.js
+++ src/data/book.js
@@ -1,10 +1,16 @@
 import chapter01 from "./book/chapter_01.json";
 import chapter02 from "./book/chapter_02.json";
+import chapter03 from "./book/chapter_03.json";
+import chapter04 from "./book/chapter_04.json";
+import chapter05 from "./book/chapter_05.json";
 
 const book = {
   chapters: [
     chapter01,
-    chapter02
+    chapter02,
+    chapter03,
+    chapter04,
+    chapter05
   ]
 };
 
