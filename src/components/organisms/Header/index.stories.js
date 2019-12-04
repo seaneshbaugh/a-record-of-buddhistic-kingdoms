95ffaf27448d1ce7f3fc5a4f85fe0a0377442713
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-12-04 15:16:44 -0600

    Fixed story that was broken because of react-router.

--- src/components/organisms/Header/index.stories.js
+++ src/components/organisms/Header/index.stories.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import { BrowserRouter } from "react-router-dom";
 import Header from "./index.js";
 
 storiesOf("organisms/Header", module)
+  .addDecorator(story => <BrowserRouter>{story()}</BrowserRouter>)
   .add("with title and subtitle", () => (<Header title="This is a title" subtitle="This is a subtitle" />));
