751a1aa959a8db960c6f9c4d442cd6562f94b655
Sean Eshbaugh <seaneshbaugh@gmail.com>
2020-01-17 17:04:41 -0600

    Forgot to actually return something for reduce.

--- src/utils/common-prop-types.js
+++ src/utils/common-prop-types.js
@@ -1,7 +1,11 @@
 import PropTypes from "prop-types";
 import { languagePrecedence } from "../store/display/selectors";
 
-export const languageProps = PropTypes.shape(languagePrecedence.reduce((props, language) => (props[language] = PropTypes.string), {}));
+export const languageProps = PropTypes.shape(languagePrecedence.reduce((props, language) => {
+  props[language] = PropTypes.string;
+
+  return props;
+}, {}));
 
 export const stringOrLanguageProps = PropTypes.oneOfType([
   PropTypes.string,
